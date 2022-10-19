2e4f23409b5bc45d5ad039b45b4e15819287618e
Arijit Paria <arijitparia2020@gmail.com>
2022-10-19 07:54:57 +0530

    add file convert feature and messegebox

--- any-to-pdf.py
+++ any-to-pdf.py
@@ -13,6 +13,7 @@ class MainThread(QThread):
     def __init__(self):
         super(MainThread, self).__init__()
 
+
 startFunction = MainThread()
 
 
@@ -25,13 +26,16 @@ class GuiStart(QMainWindow):
 
         self.input_path = ""
         self.output_path = ""
+        self.msg = QMessageBox()
 
         self.appUI.browse1.clicked.connect(self.browseFiles)
         self.appUI.browse2.clicked.connect(self.browseDir)
+        self.appUI.convertBtn.clicked.connect(self.convertFile)
         startFunction.start()
 
     def browseFiles(self):
-        self.fname = QFileDialog.getOpenFileName(self, "Open File", "C://", "files(*.txt)")
+        self.fname = QFileDialog.getOpenFileName(
+            self, "Open File", "C://")
         self.appUI.inputPath.setText(self.fname[0])
         self.appUI.outputPath.setText("/".join(self.fname[0].split("/")[:-1]))
         self.input_path = str(self.fname[0])
@@ -41,7 +45,22 @@ class GuiStart(QMainWindow):
         self.dir_name = QFileDialog.getExistingDirectory(self, "Open Folder")
         self.appUI.outputPath.setText(str(self.dir_name))
         self.output_path = str(self.dir_name)
-        
+
+    def convertFile(self):
+        try:
+            main.main(self.input_path, self.output_path)
+            # Display msg box on completing the conversion
+            self.msg.setIcon(QMessageBox.Information)
+            self.msg.setWindowTitle("File converted successfully!")
+            self.msg.setText(f"PDF file saved to \n{self.output_path}")
+            self.msg.exec_()
+
+        except Exception as e:
+            self.msg = QMessageBox()
+            self.msg.setIcon(QMessageBox.Critical)
+            self.msg.setWindowTitle("Error Occured!")
+            self.msg.setText(f"There was an error saving the PDF file.")
+            self.msg.exec_()
 
 
 app = QApplication(sys.argv)
