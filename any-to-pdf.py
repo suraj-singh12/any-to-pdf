621089c5e3631c406b18f8327da95785de328f19
Arijit Paria <arijitparia2020@gmail.com>
2022-10-19 07:31:04 +0530

    create any-to-pdf file and connect to UI

---
+++ any-to-pdf.py
@@ -0,0 +1,47 @@
+from appUI import Ui_mainWindow
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QMovie
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.uic import loadUiType
+import main
+import sys
+
+
+class MainThread(QThread):
+    def __init__(self):
+        super(MainThread, self).__init__()
+
+startFunction = MainThread()
+
+
+class GuiStart(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.appUI = Ui_mainWindow()
+        self.appUI.setupUi(self)
+
+        self.input_path = ""
+        self.output_path = ""
+
+        self.appUI.browse1.clicked.connect(self.browseFiles)
+        self.appUI.browse2.clicked.connect(self.browseDir)
+        startFunction.start()
+
+    def browseFiles(self):
+        self.fname = QFileDialog.getOpenFileName(self, "Open File", "C://", "files(*.txt)")
+        self.appUI.inputPath.setText(self.fname[0])
+        self.appUI.outputPath.setText("/".join(self.fname[0].split("/")[:-1]))
+        self.input_path = str(self.fname[0])
+        self.output_path = str("/".join(self.fname[0].split("/")[:-1]))
+
+    
+        
+
+
+app = QApplication(sys.argv)
+GUI_app = GuiStart()
+GUI_app.show()
+exit(app.exec_())
